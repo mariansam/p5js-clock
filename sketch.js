685ccbba12a7e26cb1149328fd6e44d840d10ad8
mariansam <margotka000@gmail.com>
2018-09-19 17:04:14 +0200

    Add some design

--- sketch.js
+++ sketch.js
@@ -6,17 +6,34 @@ function setup() {
 function draw() {
     clear();
     translate(width / 2, height / 2);
-    background(200);
+    background(220);
 
-    // console.log(second(), second() * 6, cos(second() * 6));
+    fill(20);
+    ellipse(0, 0, 240);
+
+    stroke(250);
+    strokeWeight(2);
+
+    for (var i = 0; i < 60; i++) {
+        var a = { x: cos(i * 6 - 90) * 100, y: sin(i * 6 - 90) * 100 };
+        point(a.x, a.y);
+    }
+
+    var sec = { x: cos(second() * 6 - 90) * 100, y: sin(second() * 6 - 90) * 100 };
+    var min = { x: cos(minute() * 6 - 90) * 100, y: sin(minute() * 6 - 90) * 100 };
+    var hur = { x: cos(hour() * 6 - 90) * 100, y: sin(hour() * 6 - 90) * 100 };
+    
+    stroke(255);
     strokeWeight(4);
-    line(0, 0, cos(second() * 6 - 90) * 100, sin(second() * 6 - 90) * 100);
-    strokeWeight(8);
-    line(0, 0, cos(minute() * 6 - 90) * 100, sin(minute() * 6 - 90) * 100);
-    strokeWeight(12);
-    line(0, 0, cos(hour() * 30 - 90) * 100, sin(hour() * 30 - 90) * 100);
 
-    strokeWeight(1);
-    line(-200, 0, 200, 0);
-    line(0, -200, 0, 200);
+    line(0, 0, sec.x, sec.y);
+    strokeWeight(8);
+    line(0, 0, min.x, min.y);
+    strokeWeight(12);
+    line(0, 0, hur.x, hur.y);
+
+    // strokeWeight(1);
+    // stroke(0);
+    // line(-200, 0, 200, 0);
+    // line(0, -200, 0, 200);
 }
